74ae2fc18f805dcf54a44d142eb15813343e853d
markab-j <markab.j.nn@gmail.com>
2025-09-05 23:47:18 +0900

    initialized

---
+++ deploy.ts
@@ -0,0 +1,14 @@
+import ghPages from 'gh-pages';
+
+await ghPages.publish(
+	'build',
+	{
+		branch: 'main',
+		nojekyll: true
+	},
+	(err) => {
+		console.error(err);
+	}
+);
+
+console.info('Published');
