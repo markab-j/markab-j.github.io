2c384fee229e3b0c8ca824c1dd36be3033e95073
markab-j <markab.j.nn@gmail.com>
2025-09-11 13:58:00 +0900

    feat: font preload

---
+++ src/routes/hooks.server.ts
@@ -0,0 +1,8 @@
+import type { Handle } from "@sveltejs/kit";
+
+export const handle: Handle = async ({ event, resolve }) => {
+  const response = await resolve(event, {
+    preload: ({ type, path }) => type === "font" || path.endsWith("woff2"),
+  });
+  return response;
+};
